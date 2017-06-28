eb44d02daf46abb8a0293f4e372122fcd4641a8f
ShaohuiQian <shhqian@gmail.com>
2017-06-27 20:57:19 -0700

    Merge

--- Game/Assets/RPB.cs
+++ Game/Assets/RPB.cs
@@ -7,16 +7,11 @@ public class RPB : MonoBehaviour {
 
 	public Transform Planet_gravityfield_big;
 	public Transform LoadingBar;
-<<<<<<< Updated upstream
 	public Transform TextIndicator;
 	public float currentAmount;
 	public float speed;
 	public bool startTimeOut = false;
-=======
-	[SerializeField] private float currentAmount;
-	[SerializeField] private float speed;
 
->>>>>>> Stashed changes
 	// Update is called once per frame
 	void Update () {
 		if (startTimeOut) {
@@ -27,16 +22,6 @@ public class RPB : MonoBehaviour {
 				TextIndicator.GetComponent<Text> ().text = "DONE!";
 			}
 			LoadingBar.GetComponent<Image> ().fillAmount = currentAmount / 100;
-
-<<<<<<< Updated upstream
-=======
-		if (currentAmount < 100) {
-			currentAmount += speed * Time.deltaTime;
-			Planet_gravityfield_big.gameObject.SetActive (true);
-			LoadingBar.gameObject.SetActive (true);
-		} else {
-			Planet_gravityfield_big.gameObject.SetActive (false);
->>>>>>> Stashed changes
 		}
 	}
 }
