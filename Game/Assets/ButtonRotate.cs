9e90cb401af90589402c0711e89b479d94b68a41
wenxiaomei <xiaomeiw6@gmail.com>
2017-06-29 17:14:37 -0700

    fix

---
+++ Game/Assets/ButtonRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonRotate : MonoBehaviour {
+
+	public bool isOn = false;
+	public Planet planet;
+	// Use this for initialization
+	void Start () {
+		planet.vRotate = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isOn) {
+			planet.vRotate = true;
+		} else {
+			planet.vRotate = false;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D col) {
+		if (col.transform.tag == "Player") {
+			isOn = !isOn;
+		}
+	}
+}
