470e6eeb4efce1710d72b50455c92cc939a9b69a
wzx520hsl <zixiwang001@gmail.com>
2017-07-06 16:04:23 -0700

    level select

---
+++ Game/Assets/Scripts/DropDownStatus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DropDownStatus : MonoBehaviour {
+    public Button btn;
+    public Dropdown dropdown;
+    public Camera cam;
+	// Use this for initialization
+	void Start () {
+        btn.onClick.AddListener(delegate { test(cam); });
+    }
+
+    void test(Camera c) {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
