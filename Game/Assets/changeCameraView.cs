1bef904577fa5922083507287674c0badec66440
wenxiaomei <xiaomeiw6@gmail.com>
2017-07-09 16:34:31 -0700

    changeViewScale

---
+++ Game/Assets/changeCameraView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeCameraView : MonoBehaviour {
+
+	public float size;
+	public Camera myCamera;
+	public Slider healthBar;
+	// Use this for initialization
+	void Start () {
+		myCamera = GetComponent<Camera> ();
+		size = myCamera.orthographicSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		size = healthBar.value;
+		myCamera.orthographicSize = size;
+	}
+}
